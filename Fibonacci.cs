1b3e843ae987929f43e773d4b75666f9ce4675db
HebertFB <hebertfbarbosa@outlook.com>
2020-01-19 20:31:56 -0300

    Exercícios de C Sharp

---
+++ Fibonacci.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long NumAnterior = 0, NumAtual = 1, fibonacci;
+            int i;
+
+            for (i = 0; i <= 50; i++)       //Por meio do laço 'for' realizar a fórmula de fibonacci até que seja satifeita as 50 primeiras.
+            {
+                fibonacci = NumAnterior + NumAtual;         //Fórmula de fibonacci (Onde o resultado consiste na soma do número anterior com o número atual.
+                Console.WriteLine("Fibonacci {0} = {1}", i, fibonacci);          //Mostrar qual a posição do fibonacci e logo a frente seu respectivo valor. 
+                NumAnterior = NumAtual;                                     /*NumAtual vale 1, portanto, NumAnterior representará o valor de maior número da
+                                                                              soma anterior na segunda repetição da Fórmula de fibonacci.*/
+                NumAtual = fibonacci;               //Será atribuído o valor de fibonacci á NumAtual.
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tabuada.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada_do_2e5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int tcinco = 5;       //Constante de tipo int para a tabuada do 5.
+            const int tdois = 2;        //Constante de tipo int para a tabuada do 2.
+            int c, d, resultado;        //Variáveis de tipo int para ambas tabuadas e resultados.
+
+
+            Console.WriteLine("\t\t░░░░░▒▒▒▒▒▒▒▓▓▓ Produced by Hebert ▓▓▓▒▒▒▒▒▒▒░░░░░");
+
+            Console.WriteLine("┌─────────────┐");
+            Console.WriteLine("│Tabuada do 5 │");   //Mostrar titulo/identifica a tabuada que será mostrada abaixo (Tabuada do 5).
+            Console.WriteLine("└─────────────┘");
+
+            for (c = 1; c <= 10; ++c)   //Por meio do laço 'for' realizar a tabuada até que seja satifeita as 10 operaçôes básicas da tabuada do 5.
+            {
+                resultado = tcinco * c;                                       //Fórmula da tabuada do 5.
+                Console.WriteLine(tcinco + " X " + c + " = " + resultado);    //Mostra resultado da tabuada do 5.
+            }
+
+            Console.WriteLine("\t\t░░░░░▒▒▒▒▒▒▒▓▓▓ Produced by Hebert ▓▓▓▒▒▒▒▒▒▒░░░░░");
+
+            Console.WriteLine("┌─────────────┐");
+            Console.WriteLine("│Tabuada do 2 │");   //Mostrar titulo/identifica a tabuada que será mostrada abaixo (Tabuada do 2).
+            Console.WriteLine("└─────────────┘");
+
+            for (d = 1; d <= 10; ++d)   //Por meio do laço 'for' realizar a tabuada até que seja satifeita as 10 operaçôes básicas da tabuada do 2.
+            {
+                resultado = tdois * d;                                        //Fórmula da tabuada do 2.
+                Console.WriteLine(tdois + " X " + d + " = " + resultado);     //Mostra resultado da tabuada do 2.
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
